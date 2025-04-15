729c3d5daad5ef17d701d474dd50157ef53b44c8
clarklindev <clarklindev@gmail.com>
2025-04-15 13:14:44 +0800

    15. Sample Products & Product List

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,20 @@
+import sampleData from '@/db/sample-data';
+import ProductList from '@/components/share/product/product-list';
+
 export const metadata = {
     title: 'home'
 };
 
 const Homepage = async () => {
-    // await new Promise((resolve) => setTimeout(resolve, 1500));
-    return <>homepage</>;
+    // await new Promise((resolve) => setTimeout(resolve, 1500));   //timeout test
+
+    return (
+        <ProductList
+            data={sampleData.products}
+            limit={4}
+            title="newest arrivals"
+        />
+    );
 };
 
 export default Homepage;
