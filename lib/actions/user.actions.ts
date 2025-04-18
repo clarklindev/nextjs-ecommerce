a817c5aadc9ce22871dff8c9f1460d0307f8b77e
clarklindev <clarklindev@gmail.com>
2025-04-18 17:22:55 +0800

    42. sign up error handling

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -5,7 +5,7 @@ import { signIn, signOut } from '@/auth';
 import { isRedirectError } from 'next/dist/client/components/redirect-error';
 import { hashSync } from 'bcrypt-ts-edge';
 import { prisma } from '@/db/prisma';
-
+import { formatError } from '../utils';
 //sign in user with credentials
 export async function signInWithCredentials(
     prevState: unknown,
@@ -66,6 +66,6 @@ export async function signUpUser(prevState: unknown, formData: FormData) {
         if (isRedirectError(error)) {
             throw error;
         }
-        return { success: false, message: 'user was not registered' };
+        return { success: false, message: formatError(error) };
     }
 }
--- lib/utils.ts
+++ lib/utils.ts
@@ -15,3 +15,30 @@ export function formatNumberWithDecimal(num: number): string {
     const [int, decimal] = num.toString().split('.');
     return decimal ? `${int}.${decimal.padEnd(2, '0')}` : `${int}.00`;
 }
+
+//format errors
+//eslint-disable-next-line
+export function formatError(error: any) {
+    if (error.name === 'ZodError') {
+        //handle zod error
+        const fieldErrors = Object.keys(error.errors).map(
+            (field) => error.errors[field].message
+        );
+
+        return fieldErrors.join('. ');
+    } else if (
+        error.name === 'PrismaClientKnownRequestError' &&
+        error.code === 'P2002'
+    ) {
+        //handle prisma
+        const field = error.meta?.target ? error.meta.target[0] : 'Field';
+        return `${
+            field.charAt(0).toUpperCase() + field.slice(1)
+        } already exists`;
+    } else {
+        //handle other errors
+        return typeof error.message === 'string'
+            ? error.message
+            : JSON.stringify(error.message);
+    }
+}
